f6292a1ac2bdc9b053371f949614d6f1834b3552
Lakitna <lakitna@gmail.com>
2017-06-08 12:16:02 +0200

    Make secrets invisible

--- client/secrets.py
+++ client/secrets.py
@@ -1,3 +1,3 @@
-def ssid = 'Kijk mama, zonder kabels!'
-def ssidpassword = 'SecretInformation78'
-def apikey = 'ToRz1XwK5ux4DZ5t6QPJ71ym@5fO647bQT^D7exs';
+def ssid = '###'
+def ssidpassword = '###'
+def apikey = '###';
