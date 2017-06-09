88641a3e2836c87322fa0b966039810d48a2666c
Lakitna <lakitna@gmail.com>
2017-06-09 11:09:04 +0200

    Content format and PHP handling

--- server/index.php
+++ server/index.php
@@ -1,24 +1,75 @@
 <?php
+@include("secrets.php");
+
 $date = getdate();
 
-if ($_GET['key'] == 'ToRz1XwK5ux4DZ5t6QPJ71ym@5fO647bQT^D7exs') {
-?>
+if (isset($_GET['key'])) {
+	if ($_GET['key'] == $secrets_api_key) {
+		if (!isset($_GET['edit'])) {
+			?>
+				{
+					"date": {
+						"y": <?php echo $date['year']; ?>,
+						"m": <?php echo $date['mon']; ?>,
+						"d": <?php echo $date['mday']; ?>
+					},
+					"time": {
+						"h": <?php echo $date['hours']; ?>,
+						"m": <?php echo $date['minutes']; ?>
+					},
+					"content":
+						<?php echo getFile("content.json"); ?>
+				}
+			<?php
+		}
+		else {
+			?>
+				<textarea style="width: 100%; height: 600px;"><?php
+					$file = json_decode(getFile("content.json"), true);
+					$revision = json_decode("{".$_GET['edit']."}", true);
+
+					foreach ($revision as $item_rev_key => $item_rev_val) { 			// Each item code in revision
+						foreach ($file[$item_rev_key] as $file_key => $file_val) { 		// Each current attribute of item in
+							foreach($revision[$item_rev_key] as $rev_key => $rev_val) { // Each attribute in revision
+								if ($file_key == $rev_key) {							// If revision attribute key is already present
+									if (gettype($rev_val) != "array") {					// If not an array do simple replace
+										$file[$item_rev_key][$file_key] = $rev_val;		// Replace
+									}
+									else {												// If it is an array, do an array merge
+										$file[$item_rev_key][$file_key] = array_merge($file[$item_rev_key][$file_key], $rev_val);
+									}
+								}
+							}
+						}
+					}
 
 
-{
-	"date": {
-		"y": <?php echo $date['year']; ?>,
-		"m": <?php echo $date['mon']; ?>,
-		"d": <?php echo $date['mday']; ?>
-	},
-	"time": {
-		"h": <?php echo $date['hours']; ?>,
-		"m": <?php echo $date['minutes']; ?>
+					$newfile = json_encode($file, true);
+					print_r($newfile);
+
+					$f = fopen("content.json", 'w+');
+					fwrite($f, $newfile);
+					fclose($f);
+				?></textarea>
+				<input type="button" value="Submit">
+			<?php
+		}
 	}
+	else { key_error(); }
 }
+else { key_error(); }
 
 
-<?php
+function getFile($file) {
+	$f = fopen($file, 'r');
+	$content = fread($f, filesize($file));
+
+	return $content;
+
+	fclose($f);
 }
-else { echo 'Invalid key'; }
+
+function key_error() {
+	echo 'ERR: Invalid API key';
+}
 ?>
---
+++ server/secrets.php
@@ -0,0 +1,3 @@
+<?php
+$secrets_api_key = 'ToRz1XwK5ux4DZ5t6QPJ71ym@5fO647bQT^D7exs';
+?>
